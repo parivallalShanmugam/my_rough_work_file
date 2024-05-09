94dc633926dcd79a89ef3dc03fd0263c54ff5edc
parivallal <vallalpari3007@gmail.com>
2024-05-09 14:07:44 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,13 @@
+#print('hello world!')
+
+class User:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password 
+
+    def login(self):
+        print('logging in....'+ self.username)
+
+    def register(self):
+        print('registreing....'+ self.username)    
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from first import User
+
+user1 = User('parivallal',12345)
+user2 = User('sasikala',47645568767)
+
+#print(user1.username)
+user1.register()
+user2.login()

ad791cd1c10bf614e5397303ac9d7461f70a9ecb
parivallal <vallalpari3007@gmail.com>
2024-05-09 16:15:02 +0530

    second commii

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from first import User
 
 user1 = User('parivallal',12345)
 user2 = User('sasikala',47645568767)
+user3 = User('jagath','vuaysg657884')
 
 #print(user1.username)
-user1.register()
-user2.login()
+#user1.register()
+
+print(user1.username)
+user2.register()

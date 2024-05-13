e96b74f87ccad5a90d1dff8c01a8ededec7be5ea
parivallal <vallalpari3007@gmail.com>
2024-05-13 11:56:45 +0530

    class inside class

--- first.py
+++ first.py
@@ -1,11 +1,28 @@
 class User:
-    def __init__(self,username,password):
-        self.username = username
-        self.password = password 
+    #count = 0
+    #def __init__(self,username,password):
+        #self.username = username
+        #self.password = password 
+        #User.count += 1
 
     def login(self):
         print('logging in....'+ self.username)
 
     def register(self):
-        print('registreing....'+ self.username)    
+        print('registreing....'+ self.username)  
 
+class Student(User):
+    def greetStudent(self):
+        print('hi student....')          
+
+class Teacher(User):
+    def greetTeacher(self):
+        print('hello teacher....')
+
+class Tempfaculty(User):
+    def greetTempfaculty(self):
+        print('nice to have you here')
+
+class Studentfaculty(Student,Teacher):
+    def Studentfaculty(self):
+        print('hello M.E Students')        
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from first import User
+from first import *
 
-user1 = User('parivallal',12345)
-user2 = User('sasikala',47645568767)
-user3 = User('jagath','vuaysg657884')
-user4 = User('pragalya',"bsg4576578")
+# user1 = User('parivallal',12345)
+# user2 = User('sasikala',47645568767)
+# user3 = User('jagath','vuaysg657884')
+# user4 = User('pragalya',"bsg4576578")
+# user5 = User('family','123256287')
 
 
 #print(user1.username)
 #user1.register()
 
-print(user1.username)
-user2.register()
+#print(user1.username)
+#user2.register()
+#print(User.count)
+
+stu1 = Student()
+tech1 = Teacher()
+temp1 = Tempfaculty()
+stufact1 = Studentfaculty()
+
+stufact1.greetStudent()
